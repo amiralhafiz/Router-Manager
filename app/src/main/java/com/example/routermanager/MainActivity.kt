d4568cd4b44eb3e7ebd3189f4beb0da0c13b8760
Amir Al Hafiz <34679121+amiralhafiz@users.noreply.github.com>
2025-06-11 20:01:37 +0800

    Enable cleartext traffic

---
+++ app/src/main/java/com/example/routermanager/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.routermanager
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.routerWebView)
+        webView.webViewClient = WebViewClient()
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("http://10.80.80.1/")
+    }
+}
