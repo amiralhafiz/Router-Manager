db593b3bb36f7f5e00da53d688d5c22e7fd77150
Amir Al Hafiz <34679121+amiralhafiz@users.noreply.github.com>
2025-07-29 10:17:13 +0800

    Revert "Add Arilia APT attribution framework (#127)" (#128)
    This reverts commit 104e407af5c6e6731feb9a8507ee333db5cd052c.

--- arilia/core/evaluator.py
+++
@@ -1,7 +0,0 @@
-"""Evaluation utilities."""
-from sklearn.metrics import classification_report
-
-
-def evaluate(true_labels, predicted_labels):
-    """Print classification report for predictions."""
-    print(classification_report(true_labels, predicted_labels))
